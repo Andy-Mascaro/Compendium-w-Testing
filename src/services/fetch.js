5238bab7a97118ef4d79ed773a5b0493ac5fe444
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-04-26 21:03:38 -0700

    added s to http in fetch

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -2,7 +2,7 @@ export async function fetchQuotes(search) {
   const url = search
     ? `https://futuramaapi.herokuapp.com/api/quotes?search=${encodeURIComponent(search)}` 
     
-    : 'http://futuramaapi.herokuapp.com/api/quotes/';
+    : 'https://futuramaapi.herokuapp.com/api/quotes/';
 const resp = await fetch (url); 
 
     const info = await resp.json();
